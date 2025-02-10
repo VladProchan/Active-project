818876d24d7d9cf1678f69bcab5eb74dc806f471
Vladyslav Prochan <vladyslavprochan@gmail.com>
2025-02-10 21:51:36 +0200

    join adaptive

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -104,7 +104,86 @@ function initSliders() {
 
 			}
 		});
-	}
+  }
+  if (document.querySelector('.join__slider')) { // Вказуємо склас потрібного слайдера
+		// Створюємо слайдер
+		new Swiper('.join__slider', { // Вказуємо склас потрібного слайдера
+			// Підключаємо модулі слайдера
+			// для конкретного випадку
+			modules: [Navigation, Pagination],
+      observer: true,
+			observeParents: true,
+      slidesPerView: 4.4,
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Ефекти
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагінація
+			
+			// pagination: {
+			// 	el: '.swiper-pagination',
+			// 	clickable: true,
+			// },
+			
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "вліво/вправо"
+			// navigation: {
+			// 	prevEl: '.swiper-button-prev',
+			// 	nextEl: '.swiper-button-next',
+			// },
+			
+			// Брейкпоінти
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 10,
+					autoHeight: true,
+        },
+        562: {
+          slidesPerView: 2,
+					spaceBetween: 20,
+        },
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1345: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			
+			// Події
+			on: {
+
+			}
+		});
+  }
+  
 }
 // Скролл на базі слайдера (за класом swiper scroll для оболонки слайдера)
 function initSlidersScroll() {
